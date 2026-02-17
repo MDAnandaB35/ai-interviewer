8b496d73201039eabb4f8f2fefe31a9d1eb15265
mdanandab35 <daffananda2020@gmail.com>
2026-02-17 22:48:09 +0700

    Added navbar, cards with mockup info (techstack, logos, score, etc), random icon and techstack logic.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import InterviewCard from "@/components/InterviewCard";
+import { dummyInterviews } from "../../../constants";
 
 const page = () => {
   return (
-    <div>
-      Home page
-    </div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Readt with AI-Powered Practices and Feedbacks</h2>
+          <p className="text-lg">
+            Prepare for your next job interview with our AI-powered platform.
+            Practice with realistic interview simulations, receive instant
+            feedback, and boost your confidence to land your dream job.
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
 
-export default page
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {... interview} key={interview.id} />
+          ))}
+
+          {/* <p>You have not taken any interview yet.</p> */}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {... interview} key={interview.id}/>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
